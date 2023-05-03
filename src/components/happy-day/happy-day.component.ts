304055c6d89e1b5e7d4914d72854ad02cc7c74c0
admirkb <kelvin@admir.tv>
2023-05-03 10:04:43 +0100

    1 file modified, 2 added

---
+++ src/components/happy-day/happy-day.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-happy-day',
+  templateUrl: './happy-day.component.html',
+  styleUrls: ['./happy-day.component.css']
+})
+export class HappyDayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
