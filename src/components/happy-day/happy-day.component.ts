714fe76d823aca4825e62a9454410c17eb9ae4ca
admirkb <kelvin@admir.tv>
2023-05-03 10:06:05 +0100

    1 file modified

--- src/components/happy-day/happy-day.component.ts
+++ src/components/happy-day/happy-day.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-happy-day',
   templateUrl: './happy-day.component.html',
   styleUrls: ['./happy-day.component.css']
